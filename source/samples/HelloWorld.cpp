9e5f04f4d8952f8e99dc7652e5cb22479f12dd6d
Sergio Bajo <sergio.bajo@xeridia.com>
2026-02-19 12:32:57 +0100

    Added samples

--- source/samples/HelloWorld.cpp
+++ source/samples/HelloWorld.cpp
@@ -16,6 +16,33 @@ void displayHello() {
     }
 }
 
+void freeFall() {
+    //Wait until we seem to be entering freefall.
+    while (uBit.accelerometer.getGesture() != ACCELEROMETER_EVT_FREEFALL) {
+        fiber_sleep(75);
+    }
+
+    uBit.serial.send("Free fall");
+    uBit.display.scroll("BANZAI!");
+}
+
+void testLog() {
+    uBit.log.setVisibility(true);
+    uBit.log.setTimeStamp(TimeStampFormat::Seconds);
+
+    //Only keep logging while we have space left.
+    for (int i = 0; i < 6 * 60 || !uBit.log.isFull(); i++) {
+
+        //Create a row with temperature data.
+        uBit.log.beginRow();
+        uBit.log.logData("Temperature", uBit.thermometer.getTemperature());
+        uBit.log.endRow();
+
+        //Wait a while.
+        uBit.sleep(10 * 1000);
+    }
+}
+
 void hello_world() { 
   uBit.init(); 
   create_fiber(displayHello);
--- source/samples/HelloWorld.h
+++ source/samples/HelloWorld.h
@@ -1,4 +1,7 @@
 #include "MicroBit.h"
+#include "MicroBitFileSystem.h"
+
 extern MicroBit uBit;
+extern MicroBitFileSystem fileSystem;
 
 void hello_world();
