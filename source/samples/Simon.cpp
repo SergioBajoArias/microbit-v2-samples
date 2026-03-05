d9517e050543e882d4218fb5635c69539fb5165a
Sergio Bajo <sergio.bajo.arias@gmail.com>
2026-03-05 13:50:41 +0100

    Simon game

--- source/samples/Simon.cpp
+++ source/samples/Simon.cpp
@@ -50,9 +50,10 @@ MicroBitImage ab_Image(ab_emoji);
 MicroBitImage tick_Image(tick_emoji);
 MicroBitImage cross_Image(cross_emoji);
 
-const int START_LEVEL = 4;
-const int LEVEL_SIZE = 3;
-int currentLevel = START_LEVEL;
+enum Turns {
+  PLAYER,
+  SIMON
+};
 
 enum SeqItems {
     A,
@@ -60,6 +61,27 @@ enum SeqItems {
     AB,
     NUMBER_OF_ITEMS
 };
+
+const int START_LEVEL = 4;
+const int LEVEL_SIZE = 3;
+int score = 0;
+int currentLevel = START_LEVEL;
+bool gameOver = false;
+Turns currentTurn = SIMON;
+std::list<SeqItems> randomSequence;
+std::list<SeqItems> userSequence;
+
+void printA() {
+  uBit.display.print(a_Image);
+}
+
+void printB() {
+  uBit.display.print(b_Image);
+}
+
+void printAB() {
+  uBit.display.print(ab_Image);
+}
 
 SeqItems generateRandom() {
     int randomPosition = std::rand() % NUMBER_OF_ITEMS;
@@ -77,11 +99,11 @@ std::list<SeqItems> generateRandomSequence() {
 void printSequence(std::list<SeqItems>& sequence) {
   for(SeqItems seqItem : sequence) {
     switch(seqItem) {
-      case A:   uBit.display.print(a_Image);
+      case A:   printA();
                 break;
-      case B:   uBit.display.print(b_Image);
+      case B:   printB();
                 break;
-      case AB:  uBit.display.print(ab_Image);
+      case AB:  printAB();
                 break;
       default:  ;
     }
@@ -90,8 +112,59 @@ void printSequence(std::list<SeqItems>& sequence) {
   }
 }
 
-void printGameOver(int score) {
+void printGameOver() {
+  uBit.display.print("GAME OVER. SCORE " + score);
+}
+
+void onButtonA(MicroBitEvent) {
+  if(currentTurn == PLAYER) {
+    userSequence.push_back(A);
+    printA();
+  }
+}
+
+void onButtonB(MicroBitEvent) {
+  if(currentTurn == PLAYER) {
+    userSequence.push_back(B);
+    printB();
+  }
+}
+
+void onButtonAB(MicroBitEvent) {
+  if(currentTurn == PLAYER) {
+    userSequence.push_back(AB);
+    printAB();
+  }
+}
+
+void verifyAnswer() {
+  if(randomSequence == userSequence) {
+    uBit.display.print(tick_Image);
+    score++;
+  } else {
+    uBit.display.print(cross_Image);
+    gameOver = true;
+    printGameOver();
+  }
+    
+  if(score % LEVEL_SIZE == 0) {
+    currentLevel++;
+  }
+}
+
+void makeSimonTurn() {
+  currentTurn = SIMON;
+  randomSequence = generateRandomSequence();
+  printSequence(randomSequence);
+  currentTurn = PLAYER;
+}
+
+void onButtonLogo(MicroBitEvent) {
+  if(currentTurn == PLAYER) {
+    verifyAnswer();
+  }
   
+  makeSimonTurn();
 }
 
 std::list<SeqItems> getUserSequence() {
@@ -99,31 +172,18 @@ std::list<SeqItems> getUserSequence() {
   return userSequence;
 }
 
-void play_simon() {
-  int score = 0;
-  bool gameOver = false;
-
-  while(!gameOver) {
-    std::list<SeqItems> randomSequence = generateRandomSequence();
-    printSequence(randomSequence);
-    std::list<SeqItems> userSequence = getUserSequence();
-    
-    if(randomSequence == userSequence) {
-      score++;
-    } else {
-      gameOver = true;
-    }
-    
-    if(score % LEVEL_SIZE == 0) {
-      currentLevel++;
-    }
-  }
-  printGameOver(score);
-}
-
 void simon() { 
   uBit.init(); 
   std::srand(std::time({}));
-  play_simon();
+  uBit.display.printAsync("SIMON");
+
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_AB, MICROBIT_BUTTON_EVT_CLICK, onButtonAB);
+  uBit.messageBus.listen(MICROBIT_ID_LOGO, MICROBIT_BUTTON_EVT_DOWN, onButtonLogo);   
+
+  while(!gameOver) {
+
+  }
   release_fiber();
 } 
