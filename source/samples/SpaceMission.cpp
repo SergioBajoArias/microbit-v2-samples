40731b1aa32f84052f0f3c12560bc7b6d37a1609
Sergio Bajo <sergio.bajo.arias@gmail.com>
2026-03-24 17:02:10 +0100

    Space mission

--- source/samples/SpaceMission.cpp
+++ source/samples/SpaceMission.cpp
@@ -1,6 +1,7 @@
 #include "SpaceMission.h"
 #include <random>
 #include <string>
+#include <list>
 
 
 const int TOP_LIMIT = 4;
@@ -8,33 +9,53 @@ const int BOTTOM_LIMIT = 0;
 const int RIGHT_LIMIT = 4;
 const int LEFT_LIMIT = 0;
 
+void hideSprite(int x, int y) {
+    uBit.display.image.setPixelValue(x, y, 0);
+}
+
+void showSprite(int x, int y) {
+    uBit.display.image.setPixelValue(x, y, 255);
+}
+
 class Sprite {
+    private:
+        int x = -1;
+        int y = -1;
+
     public:
-        int x;
-        int y;
+        void setX(int newX) {
+            hideSprite(x,y);
+            x = newX;
+            showSprite(x,y);
+        }
+
+        void setY(int newY) {
+            hideSprite(x,y);
+            y = newY;
+            showSprite(x,y);
+        }
 
         void moveUp() {
             if(y < TOP_LIMIT) {
-                y++;
+                setY(y + 1);
             }
-            
         }
 
         void moveDown() {
             if(y > BOTTOM_LIMIT) {
-                y--;
+                setY(y - 1);
             }
         }
 
         void moveLeft() {
             if(x > LEFT_LIMIT) {
-                x--;
+                setX(x - 1);
             }
         }
 
         void moveRight() {
             if(x < RIGHT_LIMIT) {
-                x++;
+                setX(x + 1);
             }
         }
 };
@@ -42,20 +63,21 @@ class Sprite {
 class Spaceship : public Sprite {
     public:
         Spaceship() {
-            y = BOTTOM_LIMIT;
-            x = 2;
+            setX(2);
+            setY(BOTTOM_LIMIT);
         }
 };
 
 class Asteroid : public Sprite {
     public:
         Asteroid() {
-            y = TOP_LIMIT;
-            x = std::rand() % RIGHT_LIMIT + 1;
+            setX(std::rand() % RIGHT_LIMIT + 1);
+            setY(TOP_LIMIT);
         }
 };
 
 Spaceship spaceship;
+std::list<Asteroid> asteroids;
 
 static void log(std::string message) {
   uBit.serial.send(message.c_str());
@@ -73,11 +95,13 @@ static void onButtonB(MicroBitEvent) {
 
 void space_mission() {
     uBit.init();
-
-    
-
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
+
+    while(true) {
+        uBit.sleep(1000);
+    }
+
     release_fiber();
 
 }
